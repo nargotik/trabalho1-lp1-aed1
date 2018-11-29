00195054bf125302afa9b739747bcd6f88156096
Daniel <Daniel@LAPTOP-55DN50VS>
2018-11-29 17:35:25 +0000

    .

---
+++ questao13/funcoes13.c
@@ -0,0 +1,10 @@
+/*!
+ * @header      main.c
+ * @abstract    Questao13.
+ * @discussion  This file contains all declarations for
+ *              foo manipulation.
+ * @author      Daniel Torres <a17442|at|alunos.ipca.pt>
+ * @link        Daniel Torres GIT <https://github.com/nargotik>
+ * @ref         some reference
+ * @version     1.0 29 de Novembro de 2018
+ */
---
+++ questao13/funcoes13.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   funcoes13.h
+ * Author: Daniel Torres <a17442|at|alunos.ipca.pt>
+ * 
+ * Created on 29 de Novembro de 2018, 17:33
+ */
+
+#ifndef FUNCOES13_H
+#define FUNCOES13_H
+
+
+
+#endif /* FUNCOES13_H */
+
